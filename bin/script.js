fc93ddfebb3e0156455e9ce2d8c2c4131393c466
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-27 18:34:30 +0000

    cambios en el script

--- bin/script.js
+++ bin/script.js
@@ -20,6 +20,7 @@ if(argv.delete || argv.d){
     exec("git config --global --unset alias.f");
     exec("git config --global --unset alias.ff");
     exec("git config --global --unset alias.fff");
+    exec("git config --global --unset alias.ffi");
 }
 
 
@@ -29,7 +30,8 @@ else if(argv.h || argv.help){
         "default install all aliases\n"+
 		"-d --delete  remove all aliases automatize -d\n"+
 		"-h --help show help\n"+
-		"-v show package version -v\n");
+		"-v show package version -v\n"+
+		"-c put your own commit -c\n");
 }
 
 else{
@@ -38,6 +40,7 @@ else{
     exec("git config --global alias.f '!git add -A && git commit -m \"no relevants changes\" && git push'");
     exec("git config --global alias.ff '!git add -A && git commit -m \"no relevants changes\" && git push && npm publish'");
     exec("git config --global alias.fff '!git add -A && git commit -m \"no relevants changes\" && npm version patch && git push && npm publish'");
+    exec("git config --global alias.ffi '!git add -A && git commit -m \" " + argv.c + " \" && npm version patch && git push && npm publish'");
 }
 
 
